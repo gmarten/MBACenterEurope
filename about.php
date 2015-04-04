d399c601e388dc55a3a18c0a3d285b162c8a0e50
Günther <gunther@mbacentereurope.eu>
2015-04-05 01:56:51 +0200

    Merge by Günther

---
+++ about.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Theme Starz">
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/about.css" type="text/css">
+    <link rel="stylesheet" href="at/gmat/css/bootstrap/bootstrap-responsive.css" type="text/css">
+    <link rel="stylesheet" href="at/gmat/css/animations/animate.css" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300" rel='stylesheet' type='text/css'>
+
+    <title>MBA Center Europe - Who We Are</title>
+
+</head>
+
+<body class="page-sub-page page-about-us">
+    <!-- Wrapper -->
+    <div class="wrapper">
+
+
+        <!-- Header -->
+        <?php include('header.php'); ?>
+        <!-- end Header -->
+
+        <!-- Breadcrumb -->
+        <div class="container">
+            <ol class="breadcrumb">
+                <li><a href="/">Home</a></li>
+                <li class="active">Who We Are</li>
+            </ol>
+        </div>
+        <!-- end Breadcrumb -->
+
+        <!-- Welcome image -->
+        <section class="container">
+                <img class="about" src="assets/img/about_welcome.png" alt="Classroom" />
+        </section>
+        <!-- End welcome image -->
+
+        <!-- Page Content -->
+        <section class="container">
+            <div class="row-fluid">
+
+                <div class="span4">
+                    <h3 class="animated delay1 fadeInDown">MBA Center Brussels</h3>
+                    <p class="animated delay1 fadeInUp">The MBA Center Brussels is one of the oldest MBA Centers in the world; it was created in 1998 and has trained thousands of students to the TOEFL, IELTS, SAT and GMAT.</p>
+                    <p class="animated delay1 fadeInUp">The MBA Center Brussels is a gateway to top European and US business schools, with hundreds of students already accepted at top universities and business schools. The MBA Center Brussels is known for its outstanding success rate in MBAs, EMBAs and MIMs with students accepted at Harvard, NYU, INSEAD, LBS, IE, IESE, HEC, ESCP Europe…</p>
+                    <p class="animated delay1 fadeInUp">In order to get such academic great results, the MBA Center has developed a unique expertise to allow its students score high on standardized tests such as the GMAT, SAT, TOEFL, TOEIC, and IELTS. The MBA Center offers courses adapted to each your needs: group classes, online classes and private tutoring.</p>
+                    <p class="animated delay1 fadeInUp">Thanks to its reputation, MBA Center is also the official partner of Educational Testing Services (TOEFL, SAT, GRE…) and the exclusive sponsor of the QS World MBA tour.</p>
+                </div>
+
+                <div class="span4">
+                    <h3 class="animated delay1 fadeInDown">The founder and CEO</h3>
+                    <p class="animated delay1 fadeInUp">
+                        <img src="assets/img/professor.jpg" alt="" align="left">
+                    </p>
+                    <p class="animated delay1 fadeInUp">
+                        Hubert Silly came in the Test Prep Industry when he first prepared the GMAT in 1992 and got a perfect score.
+                        As the only French person to have received a perfect score in the GMAT, he was rapidly contacted by publishers
+                        (Studyrama and Ellipses) who offered him to share his secrets.
+                    </p>
+                    <p class="animated delay1 fadeInUp">
+                        At that time the only Test Prep center in Paris was Kaplan. Thus, Hubert Silly decided to use his expertise to
+                        develop a test prep center that would offer more individualized and more comprehensive services: The MBA Center was born!
+                    </p>
+                    <p class="animated delay1 fadeInUp">
+                        Today, individualized and comprehensive services are common place market in the test prep industry, but Hubert Silly was
+                        the first one to understand very well where the test prep would head.
+                    </p>
+                </div>
+
+                <div class="span4">
+                    <h3 class="animated delay1 fadeInDown">A glimpse into the future</h3>
+                    <p class="animated delay1 fadeInUp">
+                        Between 1996-2009 The MBA Center developed schools in the UK, Belgium, France, Spain, Portugal, Italy, Switzerland,
+                        Germany, Austria, the Czech Republic. Since the world financial crisis, the private education sector has suffered a big
+                        change, and those prep centers that weren't able to adapt to the changing primary needs and conditions of prospective students
+                        are slowly disappearing.
+                    </p>
+                    <p class="animated delay1 fadeInUp">
+                        Today, with the competition of free online classes, The MBA Center is reassessing the model of a test Prep Center. We have
+                        understood that in the future MBA will be mainly present on online market places, institutional centers and bookshops.
+                    </p>
+                    <p class="animated delay1 fadeInUp">
+                        In order to adapt to the student's needs and keep offering an advantage we will soon offer a complete online learning
+                        platform with online diagnostic tests and access to all course material for all our students.
+                    </p>
+                </div>
+
+
+            </div>
+        </section>
+
+        <!--Google Maps-->
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.2366156238745!2d4.400245000000008!3d50.82678100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c4b5c2317f55%3A0x164fa5e119f81e8!2sICAB+Incubator!5e0!3m2!1sfr!2sfr!4v1421241801167" width="100%" height="350" scrolling="no" frameborder="0" style="border:2px; overflow:hidden;"></iframe>
+        <!--End Google Maps-->
+
+        <!-- end Page Content -->
+
+        <!-- Footer -->
+        <?php include('footer.php'); ?>
+        <!-- end Footer -->
+
+    </div>
+    <!-- end Wrapper -->
+
+    <script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/selectize.min.js"></script>
+    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+    <script type="text/javascript" src="assets/js/jQuery.equalHeights.js"></script>
+    <script type="text/javascript" src="assets/js/icheck.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.vanillabox-0.1.5.min.js"></script>
+    <script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+
+</body>
+</html>
---
+++ event-listing2.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Theme Starz">
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/selectize.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/vanillabox/vanillabox.css" type="text/css">
+
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+
+    <title>MBA center Europe Events</title>
+
+</head>
+
+<body class="page-sub-page page-events-listing">
+<!-- Wrapper -->
+<div class="wrapper">
+
+    <!-- Header -->
+    <?php include('header.php'); ?>
+    <!-- end Header -->
+
+    <!-- Breadcrumb -->
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="/">Home</a></li>
+            <li class="active">Events</li>
+        </ol>
+    </div>
+    <!-- end Breadcrumb -->
+
+    <!-- Page Content -->
+    <div id="page-content">
+        <div class="container">
+            <div class="row">
+                <!--MAIN Content-->
+                <div class="col-md-12">
+                    <div id="page-main">
+                        <section class="events" id="events">
+                            <header><h1>Events</h1></header>
+
+
+                            <div class="section-content">
+                                <article class="event">
+                                    <figure class="date">
+                                        <div class="month">jan</div>
+                                        <div class="day">27</div>
+                                    </figure>
+                                    <aside>
+                                        <header>
+                                            <a href="school-event2.php">Tuesday 27th January : Vlerick Business School</a>
+                                        </header>
+                                        <div class="additional-info"><span class="fa fa-map-marker"></span> Vlamingenstraat 83, 3000 Leuven </div>
+                                        <div class="description">
+                                            <p>Vlerick Business School regularly opens its doors to give prospective students an insight into
+                                            its Executive Education & MBA programs.
+                                            </p>
+                                        </div>
+                                        <a href="school-event2.php" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                    </aside>
+                                </article><!-- /article -->
+                                <article class="event">
+                                    <figure class="date">
+                                        <div class="month">jan</div>
+                                        <div class="day">28</div>
+                                    </figure>
+                                    <aside>
+                                        <header>
+                                            <a href="school-event3.php">Wednesday 28th January : 1-2-1 meetings with top Business Schools (17h-22h)</a>
+                                        </header>
+                                        <div class="additional-info"><span class="fa fa-map-marker"></span> ICAB/Site Arsenal</div>
+                                        <div class="description">
+                                            <p>The MBA Center Brussels will organize a 1-2-1 event with admission officers from top
+                                            business schools. The most prestigious business schools in Europe will attend the event
+                                            namely London Business School, INSEAD, Chicago, IESE, IE, HEC, ESADE, and Vlerick.
+                                            </p>
+                                        </div>
+                                        <a href="school-event3.php" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                    </aside>
+                                </article><!-- /article -->
+                                <article class="event">
+                                    <figure class="date">
+                                        <div class="month">jan</div>
+                                        <div class="day">30</div>
+                                    </figure>
+                                    <aside>
+                                        <header>
+                                            <a href="gmat800-2.php">Friday 30th January : GMAT 800 (18h30 - 21h)</a>
+                                        </header>
+                                        <div class="additional-info"><span class="fa fa-map-marker"></span> ICAB/Site Arsenal </div>
+                                        <div class="description">
+                                            <p>Each week the MBA Center students have to complete a full-length test at home (37 math
+                                            questions and 41 verbal questions). Our weekly test (GMAT 800) gives our students the
+                                            opportunity to apply the GMAT theories learnt in class and to develop their GMAT skills
+                                            (concentration, accuracy, speed…).
+                                            </p>
+                                        </div>
+                                        <a href="gmat800-2.php" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                    </aside>
+                                </article><!-- /article -->
+                                <article class="event">
+                                    <figure class="date">
+                                        <div class="month">jan</div>
+                                        <div class="day">31</div>
+                                    </figure>
+                                    <aside>
+                                        <header>
+                                            <a href="gmatu-2.php">Saturday 31st January : GMAT regular classes (9h-14h)</a>
+                                        </header>
+                                        <div class="additional-info"><span class="fa fa-map-marker"></span> ICAB/Site Arsenal </div>
+                                        <div class="description">
+                                            <p>The Saturday 31st January GMAT class will last 5 hours during which we will cover both the
+                                            math and verbal sections of the GMAT. We will use the The GMAT Companion to develop
+                                            knowledge, strategies and skills to tackle the GMAT.
+                                            </p>
+                                        </div>
+                                        <a href="gmatu-2.php" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                    </aside>
+                                </article><!-- /article -->
+                                <article class="event">
+                                    <figure class="date">
+                                        <div class="month">jan</div>
+                                        <div class="day">31</div>
+                                    </figure>
+                                    <aside>
+                                        <header>
+                                            <a href="toefl-2.php">Saturday 31st January : TOEFL advanced (14h30-17h)</a>
+                                        </header>
+                                        <div class="additional-info"><span class="fa fa-map-marker"></span> ICAB/Site Arsenal </div>
+                                        <div class="description">
+                                            <p>During the lesson your TOEFL instructor will check the students’ performance, progress, and
+                                            completion of the study plan we have set. Each student can discuss individually with the
+                                            instructor who can help him/her meet his/her objectives.
+                                            </p>
+                                        </div>
+                                        <a href="toefl-2.php" class="btn btn-framed btn-color-grey btn-small">View Details</a>
+                                    </aside>
+                                </article><!-- /article -->
+                            </div><!-- /.section-content -->
+                        </section><!-- /.events -->
+                        <div class="center">
+                            <ul class="pagination">
+                                <li><a href="/event-listing.php">1</a></li>
+                                <li class="active"><a href="">2</a></li>
+
+                            </ul>
+                        </div>
+                    </div><!-- /#page-main -->
+                </div><!-- /.col-md-8 -->
+
+            </div>
+                </div>
+                    </div>
+    <!-- end Page Content -->
+
+    <!-- Footer -->
+    <?php include('footer.php'); ?>
+    <!-- end Footer -->
+</div>
+<!-- end Wrapper -->
+
+<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/selectize.min.js"></script>
+<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.placeholder.js"></script>
+<script type="text/javascript" src="assets/js/jQuery.equalHeights.js"></script>
+<script type="text/javascript" src="assets/js/icheck.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.vanillabox-0.1.5.min.js"></script>
+<script type="text/javascript" src="assets/js/retina-1.1.0.min.js"></script>
+<script type="text/javascript" src="assets/js/custom.js"></script>
+
+</body>
+</html>
